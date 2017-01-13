251768c022fdb49462e834ee7b2b4ff9997f45ac
Christoph Heindl <christoph.heindl@gmail.com>
2017-01-13 08:38:30 +0100

    Showing graph plotting

--- example.py
+++ example.py
@@ -1,16 +1,13 @@
 import cgraph as cg
 import cgraph.simplify
+import cgraph.plot
 
 if __name__ == '__main__':
 
     x = cg.Symbol('x')
     y = cg.Symbol('y')
     z = cg.Symbol('z')
-
-    
     
     f = (x * y + 3) / (z - 2)
-    d = f.sdiff()
+    cgraph.plot.plot(f, 'graph.gv')
 
-    print(d[x])
-    print(cgraph.simplify.simplify(d[x]))
