843484fca2c97bb7824ef072b0ea6049fa2fe3cd
Heindl Christoph <christoph.heindl@profactor.at>
2017-01-26 07:53:25 +0100

    docs updated

--- sdf.py
+++ sdf.py
@@ -31,5 +31,8 @@ def circle(x, y, cx=0., cy=0., r=1.):
     """Return the signed distance function for a circle."""
     return cg.sym_sqrt((cx - x)**2 + (cy - y)**2) - r
 
+@cg.wrap_args
 def union(x, y):
+    """Return union of two signed distance functions."""
     return sym_min(x, y)
+
