66cc0308b594f6d86bbb2d9838fb21d7076b51f8
Christoph Heindl <christoph.heindl@gmail.com>
2017-01-27 13:51:40 +0100

    adding Function object

--- sdf.py
+++ sdf.py
@@ -1,4 +1,5 @@
 import cgraph as cg
+import numpy as np
   
 
 class Min(cg.Node):
@@ -35,8 +36,48 @@ def circle(x, y, cx=0., cy=0., r=1.):
     """Return the signed distance function for a circle."""
     return cg.sym_sqrt((cx - x)**2 + (cy - y)**2) - r
 
+
+@cg.wrap_args
+def line(x, y, nx=1., ny=0., d=0):
+    """Return the signed distance function for a line."""
+    return nx * x + ny * y - d
+
 @cg.wrap_args
 def union(*args):
     """Return union of multiple signed distance functions."""
     return sym_min(*args)
 
+
+cg.Node.__or__ = lambda self, other: union(self, other)
+
+
+class Function:
+
+    def __init__(self, f, symbols):
+        self.f = f
+        self.syms = [(i, s) for i, s in enumerate(symbols)]
+
+    def __call__(self, *values, with_gradient=False):
+        if len(values) == 0:
+            return cg.value(self.f, {})
+        
+        values = [np.atleast_1d(v) for v in values]
+        n = len(values[0])
+        
+        rv = np.empty(n)
+        rg = np.empty((n, len(self.syms))) if with_gradient else None
+        e = {}
+
+        if with_gradient:
+            for i in range(n):
+                e = dict([(s, values[si][i]) for si, s in self.syms])
+                g, v = cg.numeric_gradient(self.f, e, with_gradient=True)
+                rv[i] = v[self.f]                
+                rg[i][:] = [g[s] for si, s in self.syms]
+            return rv, rg
+        else:
+            for i in range(n):
+                e = dict([(s, values[si][i]) for si, s in self.syms])
+                v = cg.value(self.f, e)
+                rv[i] = v
+            return rv
