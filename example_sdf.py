5692b3b7f6f862736505ea1993bc27574907bf6f
Christoph Heindl <christoph.heindl@gmail.com>
2017-01-30 18:21:39 +0100

    using dict

--- example_sdf.py
+++ example_sdf.py
@@ -13,34 +13,39 @@ sy = cg.Symbol('y')
 f = sdf.line(sx, sy, n=[0, 1], d=-1.8) | sdf.line(sx, sy, n=[1, 1], d=-1.8) | sdf.line(sx, sy, n=[-1, 1], d=-1.8) | (sdf.subtract(sdf.circle(sx, sy, c=[0, -0.8], r=0.5), sdf.circle(sx, sy, c=[0, -0.5], r=0.5)))
 F = cg.Function(f, [sx, sy])
 
-n = 100
-stype = np.dtype([('x', float, 2), ('v', float, 2)])
-p = np.zeros(n, dtype=[
-    ('s', stype), 
-    ('m', float),
-    ('f', float, 2),
-    ('r', float),
-    ('cr', float),
-    ('cf', float)
-])
 
-p['s']['x'] = np.random.multivariate_normal([0, 1], [[0.05, 0],[0, 0.05]], n)
-p['s']['v'] = np.random.multivariate_normal([0, 0], [[0.1, 0],[0, 0.1]], n)
-p['m'] = np.random.uniform(1, 10, size=n)
-p['cr'] = 0.6
-p['cf'] = 0.4
-p['r'] = p['m'] * 0.01
+def create_particles(n=100):
+    p = {}
+    p['n'] = n
+    p['x'] = np.zeros((n, 2))
+    p['v'] = np.zeros((n, 2))
+    p['m'] = np.ones(n)
+    p['r'] = np.ones(n)
+    p['cr'] = np.full(n, 0.6)
+    p['cf'] = np.full(n, 0.6)
+    return p
+
+
+n = 100
+particles = create_particles(n=n)
+particles['x'] = np.random.multivariate_normal([0, 1], [[0.05, 0],[0, 0.05]], n)
+particles['v'] = np.random.multivariate_normal([0, 0], [[0.1, 0],[0, 0.1]], n)
+particles['m'] = np.random.uniform(1, 10, size=n)
+particles['r'] = particles['m'] * 0.01
 
 
 def gravity(p, t):
-    p['f'][:, 1] += p['m'] * -1
+    return p['m'][:, np.newaxis] * np.array([0, -1]) 
 
 
-def explicit_euler(s, sd, t, dt):
-    snew = np.empty(len(s), dtype=stype)
-    snew['x'] = s['x'] + sd['x']*dt
-    snew['v'] = s['v'] + sd['v']*dt
-    return snew
+def explicit_euler(x, v, dx, dv, t, dt):
+    xnew = np.empty(x.shape)
+    vnew = np.empty(v.shape)
+    
+    xnew = x + dx * dt
+    vnew = v + dv * dt
+
+    return xnew, vnew
 
 
 class ParticleSimulation:
@@ -48,24 +53,32 @@ class ParticleSimulation:
     def __init__(self, particles, forcegens, f, timestep=1/30, integrator=explicit_euler):
         self.f = f
         self.p = particles
+        self.p['f'] = np.zeros((self.p['n'], 2))
+
         self.forcegens = forcegens
         self.dt = timestep
         self.t = 0
-        self.int = integrator
+        self.integrator = integrator
         self.current_wall_time = None
 
-    def forces(self, t):
-        self.p['f'].fill(0.)
+    def forces(self):
+        facc = self.p['f']
+        
+        facc.fill(0.)
         for fg in self.forcegens:
-            fg(p, t)
-        return self.p['f']
+            k = fg(self.p, self.t)
+            facc += k
 
-    def dynamics(self, t):
-        s = self.p['s']
-        sd = np.empty(len(self.p), dtype=stype)        
-        sd['x'] = s['v']
-        sd['v'] = self.forces(t) / self.p['m'][:, np.newaxis]
-        return sd
+        return facc
+
+    def dynamics(self):
+        dx = np.empty((self.p['n'], 2))
+        dv = np.empty((self.p['n'], 2))
+
+        dx[:] = self.p['v']
+        dv[:] = self.forces() / self.p['m'][:, np.newaxis]
+
+        return dx, dv
 
     def update(self):
         if self.current_wall_time is None:
@@ -78,44 +91,39 @@ class ParticleSimulation:
         self.tacc += frame_time
 
         while self.tacc >= self.dt:
-            self.step()
+            self.advance()
             self.tacc -= self.dt
             self.t += self.dt
         
-    def step(self):
-        s = p['s']
-        sd = self.dynamics(self.t)
-        snew = self.int(s, sd, self.t, self.dt)
+    def advance(self):
+        xcur = self.p['x']
+        vcur = self.p['v']
 
-        x = s['x']
-        xnew = snew['x']
+        dx, dv = self.dynamics()
+        xnew, vnew = self.integrator(xcur, vcur, dx, dv, self.t, self.dt)
         
-        dbefore = self.f(x[:, 0], x[:, 1])
-        dafter, g = self.f(xnew[:, 0], xnew[:, 1], compute_gradient=True)
-        
+        d, g = self.f(xnew[:, 0], xnew[:, 1], compute_gradient=True)
+        d -= self.p['r'] # Correct for radius of particles
 
-        dafter -= p['r'] # Correct for radius
-
-        cids = np.where(dafter <= 0)[0]
+        cids = np.where(d <= 0)[0]
         if len(cids) > 0:
             # Collision response for items in collision     
             g = g[cids]
             n = g / np.linalg.norm(g, axis=1)[:, np.newaxis]
 
-            xnew = snew['x'][cids]
-            vnew = snew['v'][cids]
-            cr = p['cr'][cids, np.newaxis]
-            cf = p['cf'][cids, np.newaxis]
+            x = xnew[cids]
+            v = vnew[cids]
+            cr = self.p['cr'][cids, np.newaxis]
+            cf = self.p['cf'][cids, np.newaxis]
 
-            xnew -= (1 + cr) * dafter[cids, np.newaxis] * n
-            
-            vn = np.sum(vnew * n, axis=1)[:, np.newaxis] * n
-            vt = vnew - vn
+            vn = np.sum(v * n, axis=1)[:, np.newaxis] * n
+            vt = v - vn
 
-            snew['x'][cids] = xnew
-            snew['v'][cids] = -cr * vn + (1 - cf) * vt
+            xnew[cids] = x - (1 + cr) * d[cids, np.newaxis] * n
+            vnew[cids] = -cr * vn + (1 - cf) * vt
         
-        p['s'] = snew
+        self.p['x'][:] = xnew
+        self.p['v'][:] = vnew
 
 
 fig, ax = plt.subplots()
@@ -123,7 +131,6 @@ ax.set_xlim((-2, 2))
 ax.set_ylim((-2, 2))
 ax.set_aspect('equal')
 ax.axis('off')
-
 
 X, Y = np.mgrid[-2:2:100j, -2:2:100j]
 r, g = F(X.reshape(-1), Y.reshape(-1), compute_gradient=True)
@@ -139,16 +146,16 @@ cont = ax.contour(X, Y, v, levels=[0])
 skip = (slice(None, None, 5), slice(None, None, 5))
 ax.quiver(X[skip], Y[skip], dx[skip], dy[skip], v)
 
-actors = [plt.Circle((0,0), radius=p['r'][i]) for i in range(n)]
+actors = [plt.Circle((0,0), radius=particles['r'][i]) for i in range(n)]
 for c in actors:
     ax.add_patch(c)
 
-ps = ParticleSimulation(p, [gravity], F, timestep=1/60)
+ps = ParticleSimulation(particles, [gravity], F, timestep=1/60)
 
 def animate(i):
     ps.update()
     for i, a in enumerate(actors):
-        a.center = p['s']['x'][i, :]
+        a.center = particles['x'][i, :]
     return actors
 
 anim = animation.FuncAnimation(fig, animate,  
